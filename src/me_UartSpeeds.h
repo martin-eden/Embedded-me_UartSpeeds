83673cde71fce28fdf0d5be7252c611d2e64bb09
Ian <ian@somewhere.com>
2024-09-26 07:00:37 +0200

    [+] [UartSpeeds] Added "Arduino_Fast_Bps" and same for Esplora.

--- src/me_UartSpeeds.h
+++ src/me_UartSpeeds.h
@@ -24,7 +24,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-09-12
+  Last mod.: 2024-09-26
 */
 
 #pragma once
@@ -37,25 +37,31 @@ namespace me_UartSpeeds
   {
     Bps_300 = 300,
     Bps_600 = 600, // == prev * 2
-    Bps_1200 = 1200, // ditto
+    Bps_1200 = 1200, // ..
     Bps_2400 = 2400,
     Bps_4800 = 4800,
     Bps_9600 = 9600,
     Bps_19200 = 19200,
     Bps_38400 = 38400,
-    Bps_57600 = 57600, // ha-ha: == prev * 1.5
+    Bps_57600 = 57600, // == prev * 1.5
     Bps_115200 = 115200, // == prev * 2
-    Bps_230400 = 230400, // ditto
+    Bps_230400 = 230400, // ..
     Bps_460800 = 460800,
     Bps_921600 = 921600
   };
 
-  const StandardSpeeds Arduino_Failsafe_Bps = Bps_9600;
-  const StandardSpeeds Arduino_Normal_Bps = Bps_57600;
-  const StandardSpeeds Esplora_Normal_Bps = Bps_115200;
+  const StandardSpeeds
+    Arduino_Failsafe_Bps = Bps_9600,
+    Arduino_Normal_Bps = Bps_57600,
+    Arduino_Fast_Bps = Bps_115200,
+
+    Esplora_Failsafe_Bps = Bps_57600,
+    Esplora_Normal_Bps = Bps_115200,
+    Esplora_Fast_Bps = Bps_230400;
 }
 
 /*
   2024-03-12
   2024-04-09
+  2024-09-26
 */

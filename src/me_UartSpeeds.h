b5761eaeb11f41352e9bc7f5cf758e6bdbfbafad
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-29 19:46:01 +0200

    [!/] [Interface] Changed exported set..
    .. initially it exported list with historical speeds and
    some named pointers to it:
    
      enum StandardSpeeds:TUint_4
      {
        Bps_300 = 300,
        Bps_600 = 600, // == prev * 2
        Bps_1200 = 1200, // ditto
        Bps_2400 = 2400,
        Bps_4800 = 4800,
        Bps_9600 = 9600,
        Bps_19200 = 19200,
        Bps_38400 = 38400,
        Bps_57600 = 57600, // ha-ha: == prev * 1.5
        Bps_115200 = 115200, // == prev * 2
        Bps_230400 = 230400, // ditto
        Bps_460800 = 460800,
        Bps_921600 = 921600
      };
    
      const StandardSpeeds Arduino_Failsafe_Bps = Bps_9600;
      const StandardSpeeds Arduino_Normal_Bps = Bps_57600;
      const StandardSpeeds Esplora_Normal_Bps = Bps_115200;
    
    Note that there are no 250k, 500k, 1M and higher speeds.
    
    Speeds that seems practical for me is 9600, 57600, 115200, 250k,
    500k, 1M. And so I've changed this module for them.
    
    This change will break compilation for all snippets that use previous
    version. I don't care. It's my code. I won't stick to outdated
    solutions.

--- src/me_UartSpeeds.h
+++ src/me_UartSpeeds.h
@@ -1,30 +1,9 @@
 // Named UART speeds
 
 /*
-  Well, I don't like remembering 57600. Or 115200. Or 9600.
-
-  So I prefer named constants:
-
-    Arduino_Normal_Bps
-    Esplora_Normal_Bps
-    Arduino_Failsafe_Bps
-
-  Of course in your code you need
-
-    #include <me_UartSpeeds.h>
-    ...
-    Serial.begin(me_UartSpeeds::Arduino_Normal_Bps);
-
-  instead of
-
-    Serial.begin(57600);
-
-  Choice is yours, there are no policies, just possibilities.
-*/
-
-/*
+  Version: 2
   Author: Martin Eden
-  Last mod.: 2024-09-26
+  Last mod.: 2024-10-29
 */
 
 #pragma once
@@ -33,35 +12,19 @@
 
 namespace me_UartSpeeds
 {
-  enum StandardSpeeds:TUint_4
-  {
-    Bps_300 = 300,
-    Bps_600 = 600, // == prev * 2
-    Bps_1200 = 1200, // ..
-    Bps_2400 = 2400,
-    Bps_4800 = 4800,
-    Bps_9600 = 9600,
-    Bps_19200 = 19200,
-    Bps_38400 = 38400,
-    Bps_57600 = 57600, // == prev * 1.5
-    Bps_115200 = 115200, // == prev * 2
-    Bps_230400 = 230400, // ..
-    Bps_460800 = 460800,
-    Bps_921600 = 921600
-  };
-
-  const StandardSpeeds
-    Arduino_Failsafe_Bps = Bps_9600,
-    Arduino_Normal_Bps = Bps_57600,
-    Arduino_Fast_Bps = Bps_115200,
-
-    Esplora_Failsafe_Bps = Bps_57600,
-    Esplora_Normal_Bps = Bps_115200,
-    Esplora_Fast_Bps = Bps_230400;
+  // Here are only values that I consider practical (in 2024)
+  const TUint_4
+    Bps_9k = 9600,
+    Bps_57k = 57600,
+    Bps_115k = 115200,
+    Bps_250k = 250000,
+    Bps_500k = 500000,
+    Bps_1M = 1000000;
 }
 
 /*
   2024-03-12
   2024-04-09
   2024-09-26
+  2024-10-29
 */
